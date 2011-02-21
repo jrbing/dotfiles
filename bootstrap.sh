68b08d9e0c665efe79937105f1ef44b07dcf88b5
JR Bing <jrbing@gmail.com>
2011-02-21 15:31:46 -0500

    bump

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,3 +3,4 @@ ln -s ~/.dotfiles/vim/gvimrc ~/.gvimrc
 ln -s ~/.dotfiles/vim ~/.vim
 ln -s ~/.dotfiles/bash_profile ~/.bash_profile
 ln -s ~/.dotfiles/bashrc ~/.bashrc
+ln -s ~/.dotfiles/zshrc ~/.zshrc
