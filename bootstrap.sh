dbc6484eab4abdef4701c25b63b7aa6075a63682
JR Bing <jrbing@gmail.com>
2011-09-07 17:46:36 -0400

    Cleanup

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,5 +4,4 @@ ln -s ~/.dotfiles/vim ~/.vim
 ln -s ~/.dotfiles/bash_profile ~/.bash_profile
 ln -s ~/.dotfiles/bashrc ~/.bashrc
 ln -s ~/.dotfiles/zshrc ~/.zshrc
-ln -s ~/.dotfiles/taskrc ~/.taskrc
 ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
