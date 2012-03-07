acd7ca5e0f558707b91a6675c3d7165d6b6adc10
JR Bing <jrbing@gmail.com>
2012-03-07 14:39:34 -0500

    Added function for updating X11 forwarding

--- zsh/functions.sh
+++ zsh/functions.sh
@@ -9,3 +9,9 @@ any() {
         ps xauwww | grep -i --color=auto "[${1[1]}]${1[2,-1]}"
     fi
 }
+
+update_display() {
+  good_display=$(netstat -an | /bin/grep 0\ [0-9,:,.]*:60..\  | awk '{print $4}' | tail -n 1)
+  good_display=${good_display: -2}
+  export DISPLAY=${HOSTNAME}:${good_display}.0
+}
