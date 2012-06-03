01f9781301db659f1d7cecd40d06aba2d48853db
JR Bing <jrbing@gmail.com>
2012-06-03 17:44:55 -0400

    Updated to support additional folders

--- util/bootstrap.sh
+++ util/bootstrap.sh
@@ -1,28 +1,72 @@
 #!/usr/bin/env zsh
 
 DOTFILES=$HOME/.dotfiles
-BASE_FILES=( "ackrc" "bash_profile" "bashrc" "gemrc" "gitconfig" "gitignore" "profile" "sqlplus_completions" "tmux.conf" "zprofile" "zshrc" "wgetrc")
+BASE_FILES=( "bash_profile" "bashrc" "profile" "zprofile" "zshrc")
+ETC_FILES=("ackrc" "gemrc" "sqlplus_completions" "tmux.conf" "wgetrc")
+GIT_FILES=("gitconfig" "gitignore")
+VIM_FILES=("vimrc" "gvimrc" "vim")
 
 createBaseSymlinks () {
 for i in ${BASE_FILES[@]}
 do
-
   if [ -f ~/.${i} ] || [ -h ~/.${i} ]
     then
-      echo "Found ~/.${i}... Backing up to ~/.${i}.old";
+      printf "Found ~/.${i}... Backing up to ~/.${i}.old\n";
       cp ~/.${i} ~/.${i}.old;
       rm ~/.${i};
   fi
-
-  echo "Creating alias for ${i}"
+  printf "Creating alias for ${i}\n"
   ln -s ${DOTFILES}/${i} ~/.${i}
-
 done
 }
 
-createSymlinks
+createEtcSymlinks () {
+for i in ${ETC_FILES[@]}
+do
+  if [ -f ~/.${i} ] || [ -h ~/.${i} ]
+    then
+      printf "Found ~/.${i}... Backing up to ~/.${i}.old\n";
+      cp ~/.${i} ~/.${i}.old;
+      rm ~/.${i};
+  fi
+  printf "Creating alias for ${i}\n"
+  ln -s ${DOTFILES}/etc/${i} ~/.${i}
+done
+}
 
-ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
-ln -s ~/.dotfiles/vim/gvimrc ~/.gvimrc
-ln -s ~/.dotfiles/vim ~/.vim
+createGitSymlinks () {
+for i in ${BASE_FILES[@]}
+do
+  if [ -f ~/.${i} ] || [ -h ~/.${i} ]
+    then
+      printf "Found ~/.${i}... Backing up to ~/.${i}.old\n";
+      cp ~/.${i} ~/.${i}.old;
+      rm ~/.${i};
+  fi
+  printf "Creating alias for ${i}\n"
+  ln -s ${DOTFILES}/${i} ~/.${i}
+done
+}
+
+createVimSymlinks () {
+for i in ${BASE_FILES[@]}
+do
+  if [ -f ~/.${i} ] || [ -h ~/.${i} ]
+    then
+      printf "Found ~/.${i}... Backing up to ~/.${i}.old\n";
+      cp ~/.${i} ~/.${i}.old;
+      rm ~/.${i};
+  fi
+  printf "Creating alias for ${i}\n"
+  ln -s ${DOTFILES}/${i} ~/.${i}
+done
+}
+
+createBaseSymlinks
+createEtcSymlinks
+createGitSymlinks
+createVimSymlinks
+
+printf "Copying ssh_default settings...\n"
+cp $DOTFILES/etc/ssh_config $HOME/.ssh/config
 
