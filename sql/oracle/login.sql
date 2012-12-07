177ba1d312bf01d45b094be64e649252dc72e98f
JR Bing <jr@jrbing.com>
2012-12-07 12:32:32 -0500

    Updated sqlplus prompt

--- sql/oracle/login.sql
+++ sql/oracle/login.sql
@@ -7,10 +7,6 @@ set long 5000
 set linesize 131
 set trimspool on
 set pagesize 9999
-define sql_prompt=idle
-column user_sid new_value sql_prompt
-select lower(user) || '@' || 
-'&_CONNECT_IDENTIFIER' user_sid from dual;
-set sqlprompt '&sql_prompt> '
+set sqlprompt "_user '@' _connect_identifier > "
 set timing on
 set term on
