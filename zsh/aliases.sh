dfa9bb7697280766d83905c47b59880adc5e3fdd
JR Bing <jr@jrbing.com>
2013-01-07 10:56:14 -0500

    Changed server command over from python to Ruby

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -7,4 +7,4 @@ alias ls="ls -F"
 alias l="ls -lAh"
 alias ll="ls -l"
 alias la='ls -A'
-alias server='open http://localhost:8000 && python -m SimpleHTTPServer'
+alias server="open http://localhost:4567 && ruby -e '"'require "rack"; Rack::Handler::Thin.run(Rack::Directory.new("."), :Port => 4567)'"'"
