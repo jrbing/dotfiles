3d885875ffc7bf8fc44fc40411a9eed1f3c1a04f
JR Bing <jr@jrbing.com>
2013-10-26 19:42:27 -0400

    Added spinner

--- share/lib/bash-functions.sh
+++ share/lib/bash-functions.sh
@@ -121,3 +121,21 @@ option_enabled () {
         return 1
     fi
 }
+
+#---  FUNCTION  ----------------------------------------------------------------
+#          NAME:  spinner
+#   DESCRIPTION:  Displays a spinner while a long running job is processing
+#-------------------------------------------------------------------------------
+spinner() {
+    local pid=$1
+    local delay=0.75
+    local spinstr='|/-\'
+    while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
+        local temp=${spinstr#?}
+        printf " [%c]  " "$spinstr"
+        local spinstr=$temp${spinstr%"$temp"}
+        sleep $delay
+        printf "\b\b\b\b\b\b"
+    done
+    printf "    \b\b\b\b"
+}
