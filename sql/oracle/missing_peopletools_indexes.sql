5efb9496f2ed84143cfb4815da61be49cdc14519
JR Bing <jr@jrbing.com>
2014-07-02 10:40:45 -0400

    Whitespace

--- sql/oracle/missing_peopletools_indexes.sql
+++ sql/oracle/missing_peopletools_indexes.sql
@@ -9,7 +9,7 @@ select R.RECNAME
      , I.INDEXID
      , (case when I.INDEXTYPE = 1 then 'Key' when I.INDEXTYPE  = 2 then 'User' when I.INDEXTYPE = 3 then 'Alt' when I.INDEXTYPE = 4 then 'User' else 'Unknown' end) as PS_INDEXTYPE
   from PSRECDEFN R
- inner 
+ inner
   join PSINDEXDEFN I
     on R.RECNAME              = I.RECNAME
  where R.RECTYPE              = 0
