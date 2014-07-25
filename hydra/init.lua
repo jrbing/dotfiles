b8394fa6b5839c30c49d6fecb8ccada6bcbabb40
JR Bing <jr@jrbing.com>
2014-07-25 11:24:16 -0400

    Revert back to default configuration for the time being

--- hydra/init.lua
+++ hydra/init.lua
@@ -1,45 +1,86 @@
-#!/usr/bin/env lua
+-- Hi!
+-- Save this as ~/.hydra/init.lua and choose Reload Config from the menu
 
-require "menu"
-require "grid"
+hydra.alert("Hydra at your service", 1.5)
 
-hydra.alert "Hydra, at your service."
+-- open a repl
+--   the repl is a Lua prompt; type "print('hello world')"
+--   when you're in the repl, type "help" to get started
+--   almost all readline functionality works in the repl
+hotkey.bind({"cmd", "ctrl", "alt"}, "R", repl.open)
 
---pathwatcher.new(os.getenv("HOME") .. "/.hydra/", hydra.reload):start()
-autolaunch.set(true)
+-- save the time when updates are checked
+function checkforupdates()
+  updates.check()
+  settings.set('lastcheckedupdates', os.time())
+end
 
-local mash = {"cmd", "alt", "ctrl"}
-local mashshift = {"cmd", "alt", "shift"}
-local ctrlcmd = {"ctrl", "cmd"}
+-- show a helpful menu
+menu.show(function()
+    local updatetitles = {[true] = "Install Update", [false] = "Check for Update..."}
+    local updatefns = {[true] = updates.install, [false] = checkforupdates}
+    local hasupdate = (updates.newversion ~= nil)
 
-------------------
--- Hotkey bindings
-------------------
+    return {
+      {title = "Reload Config", fn = hydra.reload},
+      {title = "Open REPL", fn = repl.open},
+      {title = "-"},
+      {title = "About", fn = hydra.showabout},
+      {title = updatetitles[hasupdate], fn = updatefns[hasupdate]},
+      {title = "Quit Hydra", fn = os.exit},
+    }
+end)
 
---hotkey.bind(mash, ';', function() ext.grid.snap(window.focusedwindow()) end)
---hotkey.bind(mash, "'", function() fnutils.map(window.visiblewindows(), ext.grid.snap) end)
+-- move the window to the right half of the screen
+function movewindow_righthalf()
+  local win = window.focusedwindow()
+  local newframe = win:screen():frame_without_dock_or_menu()
+  newframe.w = newframe.w / 2
+  newframe.x = newframe.w -- comment this line to push it to left half of screen
+  win:setframe(newframe)
+end
 
---hotkey.bind(mash, '=', function() ext.grid.adjustwidth( 1) end)
---hotkey.bind(mash, '-', function() ext.grid.adjustwidth(-1) end)
+hotkey.new({"cmd", "ctrl", "alt"}, "L", movewindow_righthalf):enable()
 
-hotkey.bind(mashshift, 'H', function() window.focusedwindow():focuswindow_west() end)
-hotkey.bind(mashshift, 'L', function() window.focusedwindow():focuswindow_east() end)
-hotkey.bind(mashshift, 'K', function() window.focusedwindow():focuswindow_north() end)
-hotkey.bind(mashshift, 'J', function() window.focusedwindow():focuswindow_south() end)
+-- show available updates
+local function showupdate()
+  os.execute('open https://github.com/sdegutis/Hydra/releases')
+end
 
-hotkey.bind(mash, 'M', ext.grid.maximize_window)
+-- what to do when an update is checked
+function updates.available(available)
+  if available then
+    notify.show("Hydra update available", "", "Click here to see the changelog and maybe even install it", "showupdate")
+  else
+    hydra.alert("No update available.")
+  end
+end
 
---hotkey.bind(mash, 'N', ext.grid.pushwindow_nextscreen)
---hotkey.bind(mash, 'P', ext.grid.pushwindow_prevscreen)
+-- Uncomment this if you want Hydra to make sure it launches at login
+-- autolaunch.set(true)
 
---hotkey.bind(mash, 'J', ext.grid.pushwindow_down)
---hotkey.bind(mash, 'K', ext.grid.pushwindow_up)
+-- check for updates every week
+timer.new(timer.weeks(1), checkforupdates):start()
+notify.register("showupdate", showupdate)
 
---hotkey.bind(mash, 'L', ext.grid.pushwindow_right)
+-- if this is your first time running Hydra, you're launching it more than a week later, check now
+local lastcheckedupdates = settings.get('lastcheckedupdates')
+if lastcheckedupdates == nil or lastcheckedupdates <= os.time() - timer.days(7) then
+  checkforupdates()
+end
 
---hotkey.bind(mash, 'U', ext.grid.resizewindow_taller)
---hotkey.bind(mash, 'O', ext.grid.resizewindow_wider)
---hotkey.bind(mash, 'I', ext.grid.resizewindow_thinner)
 
-hotkey.bind(mash, 'X', logger.show)
-hotkey.bind(mash, "R", repl.open)
+
+
+-- I've worked hard to make Hydra useful and easy to use. I've also
+-- released it with a liberal open source license, so that you can do
+-- with it as you please. So, instead of charging for licenses, I'm
+-- asking for donations. If you find it helpful, I encourage you to
+-- donate what you believe would have been a fair price for a license:
+
+local function donate()
+  os.execute("open 'https://www.paypal.com/cgi-bin/webscr?business=sbdegutis@gmail.com&cmd=_donations&item_name=Hydra.app%20donation'")
+end
+
+hotkey.bind({"cmd", "alt", "ctrl"}, "D", donate)
+
