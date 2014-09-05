dd95eacab649cbb153d6b177eb54ad673b4268ba
JR Bing <jr@jrbing.com>
2014-09-05 11:56:08 -0400

    Modified init.lua for mjolnir

--- mjolnir/init.lua
+++ mjolnir/init.lua
@@ -1,48 +1,5 @@
--- show an alert to let you know Hydra's running
-hydra.alert("Hydra, at your service", 1.5)
 
--- open a repl with mash-R; requires https://github.com/sdegutis/hydra-cli
-hotkey.bind({"cmd", "ctrl", "alt"}, "R", repl.open)
-
--- show a helpful menu
-hydra.menu.show(function()
-    local t = {
-      {title = "Reload Config", fn = hydra.reload},
-      {title = "Open REPL", fn = repl.open},
-      {title = "-"},
-      {title = "About Hydra", fn = hydra.showabout},
-      {title = "Check for Updates...", fn = function() hydra.updates.check(nil, true) end},
-      {title = "Quit", fn = os.exit},
-    }
-
-    if not hydra.license.haslicense() then
-      table.insert(t, 1, {title = "Buy or Enter License...", fn = hydra.license.enter})
-      table.insert(t, 2, {title = "-"})
-    end
-
-    return t
-end)
-
--- move the window to the right half of the screen
-function movewindow_righthalf()
-  local win = window.focusedwindow()
-  local newframe = win:screen():frame_without_dock_or_menu()
-  newframe.w = newframe.w / 2
-  newframe.x = newframe.x + newframe.w -- comment out this line to push it to left half of screen
-  win:setframe(newframe)
-end
-
--- bind your custom function to a convenient hotkey
--- note: it's good practice to keep hotkey-bindings separate from their functions, like we're doing here
-hotkey.new({"cmd", "ctrl", "alt"}, "L", movewindow_righthalf):enable()
-
--- uncomment this line if you want Hydra to make sure it launches at login
--- hydra.autolaunch.set(true)
-
--- when the "update is available" notification is clicked, open the website
-notify.register("showupdate", function() os.execute('open https://github.com/sdegutis/Hydra/releases') end)
-
--- check for updates every week, and also right now (when first launching)
-timer.new(timer.weeks(1), hydra.updates.check):start()
-hydra.updates.check()
-
+local application = require "mjolnir.application"
+local hotkey = require "mjolnir.hotkey"
+local window = require "mjolnir.window"
+local fnutils = require "mjolnir.fnutils"
