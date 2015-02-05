454a8edd3f34c5666836163a0a4ee37dc571a82f
JR Bing <jr@jrbing.com>
2015-02-05 15:15:34 -0500

    Refactoring

--- share/valet/util.rb
+++ share/valet/util.rb
@@ -3,6 +3,13 @@ require 'fileutils'
 
 module Valet
 
+  def get_script_name(script=nil)
+    if !script
+      script = ask("Name for script: ")
+    end
+    return script
+  end
+
   def get_project_name(project=nil)
     if !project
       project = ask("Name for project: ")
@@ -10,22 +17,39 @@ module Valet
     return project
   end
 
+  def get_datafix_name(datafix=nil)
+    if !datafix
+      datafix = ask("Name for datafix: ")
+    end
+    return datafix
+  end
+
   def create_project_directory(project)
     project_directory = File.join(Dir.pwd, project)
-    say_status("info", "Creating project directory at #{project_directory}")
-    Dir.mkdir(project_directory) unless File.exists?(project_directory)
+    empty_directory(project_directory) unless File.exists?(project_directory)
     return project_directory
   end
 
   def create_project_subdirectory(subdir)
-    say_status("info", "Creating project subdirectory #{subdir}")
     subdir_path = File.join(@project_base_directory, subdir)
-    FileUtils.mkdir_p(subdir_path) unless File.exists?(subdir_path)
+    empty_directory(subdir_path) unless File.exists?(subdir_path)
     return subdir_path
   end
 
+  def create_datafix_directory(datafix)
+    datafix_directory = File.join(Dir.pwd, datafix)
+    empty_directory(datafix_directory) unless File.exists?(datafix_directory)
+    return datafix_directory
+  end
+
   def create_from_template(template_file, output_file)
-    template(template_file, File.join(@project_base_directory, output_file))
+    if @project_base_directory
+      template(template_file, File.join(@project_base_directory, output_file))
+    elsif @datafix_base_directory
+      template(template_file, File.join(@datafix_base_directory, output_file))
+    else
+      template(template_file, File.join(Dir.pwd, output_file))
+    end
   end
 
   def initialize_git_repo(path)
