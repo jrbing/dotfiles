24c42002847f04899e75d3bf3af6551702d55461
JR Bing <jr@jrbing.com>
2015-11-19 16:17:05 -0500

    Made some changes to support sqlcl

--- sql/oracle/login.sql
+++ sql/oracle/login.sql
@@ -1,12 +1,12 @@
 --Initial login settings
-set linesize 32767
-set trimspool on
-set trimout on
-set wrap off
-set termout on
-set pagesize 50000
--- set pagesize 0
-set long 5000
+-- set linesize 32767
+-- set trimspool on
+-- set trimout on
+-- set wrap off
+-- set termout on
+-- set pagesize 50000
+-- set long 5000
 define _editor=vim
 set sqlprompt '&_user@&_connect_identifier > '
+set sqlformat ansiconsole
 --set editfile /tmp/afiedt.buf
