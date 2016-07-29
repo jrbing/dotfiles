c28c2ed820673a5a60473fc59761b7401fb78363
JR Bing <jr@jrbing.com>
2016-07-29 12:23:52 -0400

    Cleanup

--- util/bootstrap.sh
+++ util/bootstrap.sh
@@ -18,29 +18,25 @@ readonly RUBY_VERSION="$(curl -sSL http://ruby.thoughtbot.com/latest)"
 readonly PROGNAME="$(basename $0)"
 source ./homebrew.sh
 
-echoinfo() {
+function echoinfo() {
   printf "${GC} *  INFO${EC}: %s\n" "$@";
 }
 
-change_shell() {
+#sudo echo "/usr/local/bin/zsh" >> /etc/shells && chsh -s /usr/local/bin/zsh
+function change_shell() {
   echoinfo "Changing shell to zsh"
   chsh -s "$(which zsh)"
 }
 
-clone_dotfiles() {
-  git clone git@github.com:jrbing/dotfiles.git "$HOME/.dotfiles"
-  cd "$HOME/.dotfiles" && git submodule init && git submodule update
-}
-
 # TODO: is this still necessary?
-fix_zsh_bug() {
+function fix_zsh_bug() {
   if [[ -f /etc/zshenv ]]; then
   echoinfo "Fixing OSX zsh environment bug ..."
     sudo mv /etc/{zshenv,zshrc}
   fi
 }
 
-install_homebrew() {
+function install_homebrew() {
   if ! command -v brew &>/dev/null; then
     echoinfo "Installing Homebrew"
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -49,62 +45,59 @@ install_homebrew() {
   fi
 }
 
-install_brews() {
+function install_brews() {
   brew tap "${TAPS[@]}" && brew install "${FORMULAS[@]}"
   brew cask "${CASKS[@]}" && brew cask alfred link
-  brew cleanup
+  brew cleanup && brew cask cleanup
 }
 
-update_homebrew() {
+function update_homebrew() {
   echoinfo "Updating Homebrew formulas"
   brew update
 }
 
-install_ruby() {
+function install_ruby() {
   echoinfo "Installing Ruby $RUBY_VERSION"
   rbenv install "$RUBY_VERSION"
 }
 
-default_ruby() {
+function default_ruby() {
   echoinfo "Setting $RUBY_VERSION as global default Ruby"
   rbenv global "$RUBY_VERSION"
   rbenv rehash
 }
 
-update_rubygems() {
+function update_rubygems() {
   echoinfo "Updating to latest Rubygems version ..."
   gem update --system
 }
 
-install_bundler() {
+function install_bundler() {
   echoinfo "Installing Bundler to install project-specific Ruby gems ..."
   gem install bundler --no-document
 }
 
-configure_bundler() {
+function configure_bundler() {
   echoinfo "Configuring Bundler for faster, parallel gem installation ..."
   number_of_cores=$(sysctl -n hw.ncpu)
   bundle config --global jobs $((number_of_cores - 1))
 }
 
-install_thor() {
+function install_thor() {
   echoinfo "Installing thor"
   gem install thor --no-document
 }
 
-#change_shell
-#clone_dotfiles
+change_shell
 #fix_zsh_bug
-#install_homebrew
-#update_homebrew
-#install_brews
-#install_ruby
-#default_ruby
+install_homebrew
+update_homebrew
+install_brews
+install_ruby
+default_ruby
 update_rubygems
 install_bundler
 configure_bundler
 install_thor
 
 #TODO: add rbenv shims to path
-
-#sudo echo "/usr/local/bin/zsh" >> /etc/shells && chsh -s /usr/local/bin/zsh
