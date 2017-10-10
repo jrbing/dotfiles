53c53efe6714c32f0e10eed0ed4d76b920292e4b
JR Bing <jr@jrbing.com>
2017-10-10 19:33:04 -0400

    Added additional packages and cleaned up bootstrap script

--- util/bootstrap.sh
+++ util/bootstrap.sh
@@ -12,43 +12,25 @@
 
 trap 'ret=$?; test $ret -ne 0 && printf "failed\n\n" >&2; exit $ret' EXIT
 
-set -e
+set -e          # Exit immediately on error
+set -u          # Treat unset variables as an error
+set -o pipefail # Prevent errors in a pipeline from being masked
+IFS=$'\n\t'     # Set the internal field separator to a tab and newline
 
-readonly RUBY_VERSION="$(curl -sSL http://ruby.thoughtbot.com/latest)"
 readonly PROGNAME="$(basename $0)"
-source ./homebrew.sh
+readonly PROGDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-function echoinfo() {
-  printf "${GC} *  INFO${EC}: %s\n" "$@";
-}
-
-#sudo echo "/usr/local/bin/zsh" >> /etc/shells && chsh -s /usr/local/bin/zsh
-function change_shell() {
-  echoinfo "Changing shell to zsh"
-  chsh -s "$(which zsh)"
-}
-
-# TODO: is this still necessary?
-function fix_zsh_bug() {
-  if [[ -f /etc/zshenv ]]; then
-  echoinfo "Fixing OSX zsh environment bug ..."
-    sudo mv /etc/{zshenv,zshrc}
-  fi
-}
+source "${PROGDIR}/../share/lib/bash-functions.sh"
+source "${PROGDIR}/homebrew.sh"
 
 function install_homebrew() {
+  echoinfo "Installing homebrew"
   if ! command -v brew &>/dev/null; then
     echoinfo "Installing Homebrew"
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
   else
     echoinfo "Homebrew already installed"
   fi
-}
-
-function install_brews() {
-  brew tap "${TAPS[@]}" && brew install "${FORMULAS[@]}"
-  brew cask "${CASKS[@]}" && brew cask alfred link
-  brew cleanup && brew cask cleanup
 }
 
 function update_homebrew() {
@@ -56,48 +38,72 @@ function update_homebrew() {
   brew update
 }
 
-function install_ruby() {
-  echoinfo "Installing Ruby $RUBY_VERSION"
-  rbenv install "$RUBY_VERSION"
+function install_taps() {
+  echoinfo "Installing additional Homebrew taps"
+  brew tap "${TAPS[@]}"
 }
 
-function default_ruby() {
-  echoinfo "Setting $RUBY_VERSION as global default Ruby"
-  rbenv global "$RUBY_VERSION"
-  rbenv rehash
+function install_formulas() {
+  echoinfo "Installing homebrew formulas"
+  brew install "${FORMULAS[@]}"
 }
 
-function update_rubygems() {
-  echoinfo "Updating to latest Rubygems version ..."
-  gem update --system
+function install_casks() {
+  echoinfo "Installing homebrew casks"
+  brew cask "${CASKS[@]}"
 }
 
-function install_bundler() {
-  echoinfo "Installing Bundler to install project-specific Ruby gems ..."
-  gem install bundler --no-document
+function cleanup_homebrew() {
+  echoinfo "Cleaning up homebrew formulas and casks"
+  brew cleanup && brew cask cleanup
 }
 
-function configure_bundler() {
-  echoinfo "Configuring Bundler for faster, parallel gem installation ..."
-  number_of_cores=$(sysctl -n hw.ncpu)
-  bundle config --global jobs $((number_of_cores - 1))
+function change_shell() {
+  #sudo echo "/usr/local/bin/zsh" >> /etc/shells && chsh -s /usr/local/bin/zsh
+  echoinfo "Changing shell to zsh"
+  chsh -s /usr/local/bin/zsh
 }
 
-function install_thor() {
-  echoinfo "Installing thor"
-  gem install thor --no-document
-}
+#function install_ruby() {
+  #echoinfo "Installing Ruby $RUBY_VERSION"
+  #rbenv install "$RUBY_VERSION"
+#}
 
-change_shell
-#fix_zsh_bug
+#function default_ruby() {
+  #echoinfo "Setting $RUBY_VERSION as global default Ruby"
+  #rbenv global "$RUBY_VERSION"
+  #rbenv rehash
+#}
+
+#function update_rubygems() {
+  #echoinfo "Updating to latest Rubygems version"
+  #gem update --system
+#}
+
+#function install_gems() {
+  #echoinfo "Installing regularly used ruby gems"
+  #gem install bundler --no-document
+  #gem install thor --no-document
+  #gem install pry --no-document
+#}
+
+#function configure_bundler() {
+  #echoinfo "Configuring Bundler for parallel gem installation"
+  #local number_of_cores=$(sysctl -n hw.ncpu)
+  #bundle config --global jobs $((number_of_cores - 1))
+#}
+
 install_homebrew
 update_homebrew
-install_brews
-install_ruby
-default_ruby
-update_rubygems
-install_bundler
-configure_bundler
-install_thor
+install_taps
+install_formulas
+install_casks
+cleanup_homebrew
 
-#TODO: add rbenv shims to path
+#install_ruby
+#default_ruby
+#update_rubygems
+#install_gems
+#configure_bundler
+
+#change_shell
--- util/homebrew.sh
+++ util/homebrew.sh
@@ -1,57 +1,102 @@
 #!/usr/bin/env bash
+# vim: softtabstop=2 shiftwidth=2 expandtab fenc=utf-8 spelllang=en ft=sh
+# shellcheck disable=2034
 
 # Homebrew taps
 TAPS=(
-    homebrew/binary
-    homebrew/dupes
-    caskroom/cask
+  homebrew/binary
+  homebrew/completions
+  homebrew/dupes
+  homebrew/services
+  caskroom/cask
+  buo/cask-upgrade
+  kryptco/tap
+  moncho/dry
+  neovim/neovim
+  yudai/gotty
 )
 
 # Homebrew Formulas
 FORMULAS=(
-    ack
-    ansible
-    aria2
-    blink1
-    cheat
-    coreutils
-    ctags
-    direnv
-    findutils
-    git
-    htop-osx
-    hub
-    imagemagick
-    keybase
-    macvim --with-override-system-vim
-    mosh
-    ngrok
-    openssl
-    optipng
-    pandoc
-    pigz
-    rbenv
-    rbenv-gem-rehash
-    readline
-    reattach-to-user-namespace
-    rlwrap
-    ruby-build
-    shellcheck
-    ssh-copy-id
-    the_silver_searcher
-    tmux
-    tree
-    watchman
-    wget
-    zopfli
-    zsh
+  ack
+  ansible
+  ansible-cmdb
+  ansible-lint
+  aria2
+  astyle
+  bash
+  blink1
+  cheat
+  clipper
+  cmake
+  coreutils
+  ctags
+  ctop
+  curl
+  docker-completion
+  direnv
+  dry
+  editorconfig
+  findutils
+  git
+  git-flow-avh
+  git-lfs
+  glide
+  go
+  gpg-agent
+  gradle
+  grep
+  htop
+  hub
+  hugo
+  imagemagick
+  jq
+  keybase
+  kr
+  macvim --with-override-system-vim
+  mas
+  md5sha1sum
+  mosh
+  neovim
+  openssl
+  optipng
+  packer
+  packer-completion
+  pandoc
+  pigz
+  pinentry
+  pinentry-mac
+  python
+  python3
+  rbenv
+  readline
+  reattach-to-user-namespace
+  rlwrap
+  rsync
+  ruby-build
+  shellcheck
+  sassc
+  shellcheck
+  sqlite
+  ssh-copy-id
+  terraform
+  the_silver_searcher
+  tldr
+  tmux
+  tmuxinator-completion
+  tree
+  watchman
+  wget
+  yank
+  yarn
+  zopfli
+  zsh
 )
 
 # Homebrew casks
 CASKS=(
-    basictex
-    growlnotify
-    vagrant
-    virtualbox
-    xquartz
+  basictex
+  vagrant
+  virtualbox
+  xquartz
 )
