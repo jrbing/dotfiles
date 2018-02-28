a71cbdcf362174d2bfc52aa324facab0622e50a1
JR Bing <jr@jrbing.com>
2018-02-28 14:09:52 -0500

    Cleanup admin module

--- powershell/modules/admin.psm1
+++ powershell/modules/admin.psm1
@@ -1,21 +1,45 @@
 ﻿
-# TODO: add help
 Function Invoke-Elevated {
-    $file, [string]$arguments = $args;
-    $psi = new-object System.Diagnostics.ProcessStartInfo $file;
-    $psi.Arguments = $arguments;
-    $psi.Verb = "runas";
-    $psi.WorkingDirectory = get-location;
-    [System.Diagnostics.Process]::Start($psi);
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory = $true)][ValidateNotNullOrEmpty()][File] $FileName
+    )
+    Process {
+        Try {
+            $psi = New-Object System.Diagnostics.ProcessStartInfo $FileName
+            $psi.Verb = "runas"
+            $psi.WorkingDirectory = Get-Location
+            [System.Diagnostics.Process]::Start($psi)
+        }
+        Catch {
+            Write-Error -Message $_.Exception
+            Break
+        }
+    }
 }
 
-# TODO: add help
-function Test-IsAdmin {
+Function Test-IsAdmin {
     Try {
         $identity = [Security.Principal.WindowsIdentity]::GetCurrent()
         $principal = New-Object Security.Principal.WindowsPrincipal -ArgumentList $identity
         Return $principal.IsInRole( [Security.Principal.WindowsBuiltInRole]::Administrator )
     } Catch {
         Throw "Failed to determine if the current user has elevated privileges. The error was: '{0}'." -f $_
+    }
+}
+
+Function Which {
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory = $true)][ValidateNotNullOrEmpty()][String] $Name
+    )
+    Process {
+        Try {
+            Get-Command $Name | Select-Object -ExpandProperty Definition
+        }
+        Catch {
+            Write-Error -Message $_.Exception
+            Break
+        }
     }
 }
