a7824eb3a47b6e9c240bbe823728643a41f401fb
JR Bing <jr@jrbing.com>
2018-06-22 11:18:29 -0400

    Switch to single quotes

--- zsh/packages.zsh
+++ zsh/packages.zsh
@@ -2,46 +2,52 @@
 # zplug packages
 #
 
-zplug "clvv/fasd", \
+zplug 'clvv/fasd', \
+  depth:10, \
   as:command, \
   use:'fasd'
 
-zplug "sorin-ionescu/prezto", \
+zplug 'sorin-ionescu/prezto', \
+  depth:10, \
   as:plugin, \
   use:init.zsh, \
   hook-build:"ln -s ${ZPLUG_ROOT}/repos/sorin-ionescu/prezto ${ZDOTDIR:-$HOME}/.zprezto"
 
-zplug "belak/prezto-contrib", \
+zplug 'belak/prezto-contrib', \
+  depth:10, \
   as:plugin
 
-zplug "asdf-vm/asdf", \
+zplug 'asdf-vm/asdf', \
+  depth:10, \
   as:plugin, \
   use:'asdf.sh'
 
-zplug "Tarrasch/zsh-bd", \
+zplug 'Tarrasch/zsh-bd', \
+  depth:10, \
   as:plugin
 
-zplug "rimraf/k", \
+zplug 'rimraf/k', \
+  depth:10, \
   as:plugin
 
-zplug "b4b4r07/httpstat", \
+zplug 'b4b4r07/httpstat', \
   as:command, \
   use:'(*).sh', \
   rename-to:'$1'
 
-zplug "genuinetools/reg", \
-  from:gh-r, \
-  as:command
+zplug 'genuinetools/reg', \
+  as:command, \
+  from:gh-r
 
-zplug "genuinetools/audit", \
-  from:gh-r, \
-  as:command
+zplug 'genuinetools/audit', \
+  as:command, \
+  from:gh-r
 
-zplug "genuinetools/pepper", \
-  from:gh-r, \
-  as:command
+zplug 'genuinetools/pepper', \
+  as:command, \
+  from:gh-r
 
-zplug "drwetter/testssl.sh", \
+zplug 'drwetter/testssl.sh', \
   as:command, \
   use:'testssl.sh', \
   rename-to:'testssl'
