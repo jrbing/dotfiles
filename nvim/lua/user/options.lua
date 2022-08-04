d11d970e7917044c541ed14700dadfd6ecc72aa0
JR Bing <jr@jrbing.com>
2022-08-04 10:53:03 -0400

    Start migrating over settings from vim

--- nvim/lua/user/options.lua
+++ nvim/lua/user/options.lua
@@ -38,3 +38,105 @@ vim.opt.fillchars.eob=" "
 vim.opt.shortmess:append "c"
 vim.opt.whichwrap:append("<,>,[,],h,l")
 vim.opt.iskeyword:append("-")
+
+--if has('multi_byte')
+  --if &termencoding ==# ''
+    --let &termencoding = &encoding
+  --endif
+  --set encoding=utf-8
+  --scriptencoding utf-8
+  --setglobal fileencoding=utf-8
+  --"setglobal bomb
+  --set fileencodings=ucs-bom,utf-8,latin1
+--endif
+
+--set autoindent
+--set breakindent
+--set showbreak=↳
+--set showmode
+--set showcmd
+--set hidden
+--set visualbell
+--set ttyfast
+--set ruler
+--set backspace=indent,eol,start
+--set number
+--set laststatus=2
+--set history=1000
+--set cpoptions+=J
+--set matchtime=3
+--set splitbelow
+--set splitright
+--set fillchars=diff:⣿
+--set ttimeout
+--set notimeout
+--set nottimeout
+--set autowrite
+--set shiftround
+--set autoread
+--set title
+--set mouse=a
+--set mousehide
+--set synmaxcol=200                               " Don't try to highlight lines longer than 200 characters.
+--set shortmess+=filmnrxoOtT                      " Abbrev. of messages (avoids 'hit enter')
+--set viewoptions=folds,options,cursor,unix,slash " Better Unix / Windows compatibility
+--set iskeyword-=.                    " '.' is an end of word designator
+--set iskeyword-=#                    " '#' is an end of word designator
+--set iskeyword-=-                    " '-' is an end of word designator
+--set backupdir=$HOME/.vim/tmp/backup// " backup directory location
+--set directory=$HOME/.vim/tmp/swap//   " swap file location
+--set backup                            " enable backups
+--set noswapfile                        " disable swapfiles
+
+--" Wildmenu completion
+--set wildmenu
+--set wildmode=list:longest
+--set wildignore+=.hg,.git,.svn                    " version control
+--set wildignore+=*.aux,*.out,*.toc                " LaTeX intermediate files
+--set wildignore+=*.jpg,*.bmp,*.gif,*.png,*.jpeg   " binary images
+--set wildignore+=*.o,*.obj,*.exe,*.dll,*.manifest " compiled object files
+--set wildignore+=*.spl                            " compiled spelling word lists
+--set wildignore+=*.sw?                            " Vim swap files
+--set wildignore+=*.DS_Store                       " OSX temp files
+--" Use modeline overrides
+--set modeline
+--set modelines=5
+--" Set par as the paragraph formatting program to use
+--if executable('par')
+  --set formatprg=par
+--endif
+--" Set folding method, but disable by default
+--set foldmethod=syntax
+--set nofoldenable
+--set nowrap
+--set expandtab
+--set tabstop=2
+--set shiftwidth=2
+--set softtabstop=2
+--set textwidth=80
+--set formatoptions=qrn1j
+--if v:version >= 800
+  --" True color support
+  --set termguicolors
+--endif
+--if v:version >= 730
+  --set norelativenumber
+  --set undofile
+  --set undoreload=10000
+  --set undodir=$HOME/.vim/tmp/undo//     " undo files
+  --set colorcolumn=+1
+--endif
+--set ignorecase
+--set smartcase
+--set incsearch
+--set showmatch
+--set hlsearch
+--set gdefault
+
+--set scrolloff=3
+--set sidescroll=1
+--set sidescrolloff=10
+
+--set virtualedit+=block
+--set list
+--set listchars=trail:·,precedes:«,extends:»,tab:▸\
