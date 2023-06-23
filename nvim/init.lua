28ced56327f9c3f6650f90c83d33c6317bc6514b
JR Bing <jr@jrbing.com>
2023-06-23 11:12:21 -0400

    Only track NvChad custom config for nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,21 +1,7 @@
-require "core"
+-- local autocmd = vim.api.nvim_create_autocmd
 
-local custom_init_path = vim.api.nvim_get_runtime_file("lua/custom/init.lua", false)[1]
-
-if custom_init_path then
-  dofile(custom_init_path)
-end
-
-require("core.utils").load_mappings()
-
-local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
-
--- bootstrap lazy.nvim!
-if not vim.loop.fs_stat(lazypath) then
-  require("core.bootstrap").gen_chadrc_template()
-  require("core.bootstrap").lazy(lazypath)
-end
-
-dofile(vim.g.base46_cache .. "defaults")
-vim.opt.rtp:prepend(lazypath)
-require "plugins"
+-- Auto resize panes when resizing nvim window
+-- autocmd("VimResized", {
+--   pattern = "*",
+--   command = "tabdo wincmd =",
+-- })
