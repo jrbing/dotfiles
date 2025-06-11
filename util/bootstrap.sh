e786ecd53dc7d2659305818b93ccf4216eb4e223
JR Bing <jr@jrbing.com>
2025-06-11 13:33:44 -0400

    Changes to Brewfile

--- util/bootstrap.sh
+++ util/bootstrap.sh
@@ -17,7 +17,6 @@ set -u          # Treat unset variables as an error
 set -o pipefail # Prevent errors in a pipeline from being masked
 IFS=$'\n\t'     # Set the internal field separator to a tab and newline
 
-readonly PROGNAME="$(basename $0)"
 readonly PROGDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 source "${PROGDIR}/../share/lib/bash-functions.sh"
@@ -26,7 +25,6 @@ function install_homebrew() {
   echoinfo "Installing homebrew"
   if ! command -v brew &>/dev/null; then
     echoinfo "Installing Homebrew"
-    #/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   else
     echoinfo "Homebrew already installed"
@@ -40,7 +38,7 @@ function update_homebrew() {
 
 function install_from_brewfile() {
   echoinfo "Installing from Brewfile"
-  cd ~/.dotfiles && brew bundle
+  cd ~/.dotfiles/util && brew bundle
 }
 
 function cleanup_homebrew() {
@@ -49,7 +47,6 @@ function cleanup_homebrew() {
 }
 
 function change_shell() {
-  #sudo echo "/usr/local/bin/zsh" >> /etc/shells && chsh -s /usr/local/bin/zsh
   echoinfo "Changing shell to zsh"
   chsh -s /usr/local/bin/zsh
 }
@@ -58,5 +55,4 @@ install_homebrew
 update_homebrew
 install_from_brewfile
 cleanup_homebrew
-
-#change_shell
+change_shell
