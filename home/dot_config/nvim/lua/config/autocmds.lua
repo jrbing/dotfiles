855e4efec86137ef6dde84eb9f6992b80e28cc04
JR Bing <jrbing@users.noreply.github.com>
2026-07-18 20:59:44 -0400

    Added lazyvim starter files

---
+++ home/dot_config/nvim/lua/config/autocmds.lua
@@ -0,0 +1,33 @@
+-- Autocmds are automatically loaded on the VeryLazy event
+-- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+--
+-- Add any additional autocmds here
+-- with `vim.api.nvim_create_autocmd`
+--
+-- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
+-- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Last location restore, resize, autosave on focus lost
+-- augroup("miscellaneous", { clear = true })
+
+-- autocmd("FocusLost", {
+  -- group = "miscellaneous",
+  -- pattern = "*",
+  -- command = "silent! wall",
+-- })
+-- autocmd("VimResized", {
+  -- group = "miscellaneous",
+  -- pattern = "*",
+  -- command = "wincmd =",
+-- })
+
+-- autocmd("BufReadPost", {
+  -- group = "miscellaneous",
+  -- pattern = "*",
+  -- callback = function()
+    -- local line = vim.fn.line "'\""
+    -- if line > 0 and line <= vim.fn.line "$" then
+      -- vim.cmd [[normal! g`"zvzz]]
+    -- end
+  -- end,
+-- })
---
+++ home/dot_config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,76 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+map("n", ";", ":", { desc = "CMD enter command mode" })
+map("i", "jk", "<ESC>")
+
+-- Bubble lines up/down
+map("n", "<C-Up>", "[e", { desc = "Bubble line up" })
+map("n", "<C-Down>", "]e", { desc = "Bubble line down" })
+map("v", "<C-Up>", "[egv", { desc = "Bubble selection up" })
+map("v", "<C-Down>", "]egv", { desc = "Bubble selection down" })
+
+-- Split navigation
+map("n", "<C-J>", "<C-W>j<C-W>_", { desc = "Split down" })
+map("n", "<C-K>", "<C-W>k<C-W>_", { desc = "Split up" })
+map("n", "<C-L>", "<C-W>l<C-W>_", { desc = "Split right" })
+map("n", "<C-H>", "<C-W>h<C-W>_", { desc = "Split left" })
+
+-- File/path helpers
+map("n", "<leader>e", ':e <C-R>=expand("%:p:h") . "/" <CR>', { desc = "Edit in current dir" })
+map("n", "<leader>v", "V`]", { desc = "Re-select last paste" })
+
+-- Telescope in place of CtrlP
+map("n", "<leader>b", telescope.buffers, { desc = "Buffers (CtrlPBuffer)" })
+map("n", "<A-t>", telescope.find_files, { desc = "Find files" })
+
+-- Window sizing
+-- map("n", "<leader>=", "<C-w>=", { desc = "Equalize splits" })
+-- map("i", "<leader>=", "<Esc><C-w>=", { desc = "Equalize splits" })
+
+-- Alignment helpers
+--map("n", "<leader>Al", ":left<cr>", { desc = "Align left" })
+--map("n", "<leader>Ac", ":center<cr>", { desc = "Align center" })
+--map("n", "<leader>Ar", ":right<cr>", { desc = "Align right" })
+--map("v", "<leader>Al", ":left<cr>", { desc = "Align left" })
+--map("v", "<leader>Ac", ":center<cr>", { desc = "Align center" })
+--map("v", "<leader>Ar", ":right<cr>", { desc = "Align right" })
+
+map("n", ",gf", ":vertical botright wincmd f<CR>", { desc = "Open file in vsplit" })
+
+-- Yank to EOL for consistency with C/D
+map("n", "Y", "y$", { desc = "Yank to end of line" })
+
+-- Horizontal scroll shortcuts
+map({ "n", "v" }, "zl", "zL", { desc = "Scroll right" })
+map({ "n", "v" }, "zh", "zH", { desc = "Scroll left" })
+
+-- Clear search highlights
+map("n", "<leader><space>", "<cmd>noh<cr>", { desc = "Clear search" })
+
+-- Keep search matches centered and pulse cursorline
+map("n", "n", function()
+  vim.cmd "normal! nzzzv"
+  utils.pulse_cursorline()
+end, { desc = "Next search + pulse" })
+map("n", "N", function()
+  vim.cmd "normal! Nzzzv"
+  utils.pulse_cursorline()
+end, { desc = "Prev search + pulse" })
+
+-- Function keys
+map({ "i", "n", "v" }, "<F1>", "<Esc>", { desc = "Escape on F1" })
+map("n", "<F4>", function()
+  require("conform").format { async = false, lsp_fallback = true }
+end, { desc = "Format buffer" })
+map("n", "<F5>", utils.strip_trailing_whitespace, { desc = "Trim trailing whitespace" })
+
+-- Wipeout unused buffers
+map("n", "<leader>wo", "<cmd>Wipeout<CR>", { desc = "Close non-tab buffers" })
+
+-- Toggle verbose logging
+map("n", "<leader>tv", utils.toggle_verbose, { desc = "Toggle verbose logging" })
+
+-- nvim-tree
+map("n", "<leader>n", ":NvimTreeToggle<cr>", { desc = "Toggle NvimTree" })
