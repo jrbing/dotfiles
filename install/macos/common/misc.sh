7db78ba15db0b9fba15a8c2fb7fed1a10ef3ce29
JR Bing <jrbing@users.noreply.github.com>
2026-08-21 15:00:12 -0400

    chore: remove Google Chrome from homebrew install scripts
    - Remove google-chrome cask from CASK_PACKAGES in misc.sh
    - Remove Chrome from persistent dock apps in defaults.sh
    - Remove commented-out Chrome entries from kill_affected_applications

--- install/macos/common/misc.sh
+++ install/macos/common/misc.sh
@@ -9,73 +9,72 @@
 set -Eeuo pipefail
 
 if [ "${DOTFILES_DEBUG:-}" ]; then
-    set -x
+	set -x
 fi
 
 readonly BREW_PACKAGES=(
-    aria2
-    atuin
-    clipper
-    cmake
-    colima
-    direnv
-    docker
-    docker-buildx
-    eza
-    ffmpeg
-    gh
-    git-delta
-    git-filter-repo
-    gnupg
-    go
-    go-task
-    helm
-    htop
-    icdiff
-    imagemagick
-    just
-    krew
-    kubectx
-    kubernetes-cli
-    mise
-    mole
-    nmap
-    node
-    opencode
-    pinentry-mac
-    reattach-to-user-namespace
-    ripgrep
-    starship
-    stern
-    terminal-notifier
-    tmux
-    topgrade
-    watchexec
-    wget
-    yt-dlp
-    zoxide
+	aria2
+	atuin
+	clipper
+	cmake
+	colima
+	direnv
+	docker
+	docker-buildx
+	eza
+	ffmpeg
+	gh
+	git-delta
+	git-filter-repo
+	gnupg
+	go
+	go-task
+	helm
+	htop
+	icdiff
+	imagemagick
+	just
+	krew
+	kubectx
+	kubernetes-cli
+	mise
+	mole
+	nmap
+	node
+	opencode
+	pinentry-mac
+	reattach-to-user-namespace
+	ripgrep
+	starship
+	stern
+	terminal-notifier
+	tmux
+	topgrade
+	watchexec
+	wget
+	yt-dlp
+	zoxide
 )
 
 # readonly BREW_TAPS=(
-    # #manaflow-ai/cmux
+# #manaflow-ai/cmux
 # )
 
 readonly CASK_PACKAGES=(
-    1password
-    1password-cli
-    alfred
-    font-inconsolata-dz-for-powerline
-    font-inconsolata-go-nerd-font
-    font-inconsolata-nerd-font
-    google-chrome
-    iina
-    iterm2
-    lens
-    macvim-app
-    serial
-    syncthing-app
-    vimr
-    vlc
+	1password
+	1password-cli
+	alfred
+	font-inconsolata-dz-for-powerline
+	font-inconsolata-go-nerd-font
+	font-inconsolata-nerd-font
+	iina
+	iterm2
+	lens
+	macvim-app
+	serial
+	syncthing-app
+	vimr
+	vlc
 )
 
 #
@@ -83,9 +82,9 @@ readonly CASK_PACKAGES=(
 # @arg $1 string Package or cask name.
 #
 function is_brew_package_installed() {
-    local package="$1"
+	local package="$1"
 
-    brew list "${package}" &> /dev/null
+	brew list "${package}" &>/dev/null
 }
 
 #
@@ -93,86 +92,86 @@ function is_brew_package_installed() {
 # @arg $1 string Tap name.
 #
 function is_brew_tap_installed() {
-    local tap="$1"
+	local tap="$1"
 
-    brew tap | grep --fixed-strings --line-regexp --quiet "${tap}"
+	brew tap | grep --fixed-strings --line-regexp --quiet "${tap}"
 }
 
 #
 # @description Install every missing tap from `BREW_TAPS` unless running in CI.
 #
 function install_brew_taps() {
-    if "${CI:-false}"; then
-        return 0
-    fi
+	if "${CI:-false}"; then
+		return 0
+	fi
 
-    local missing_taps=()
-    local tap
+	local missing_taps=()
+	local tap
 
-    for tap in "${BREW_TAPS[@]}"; do
-        if ! is_brew_tap_installed "${tap}"; then
-            missing_taps+=("${tap}")
-        fi
-    done
+	for tap in "${BREW_TAPS[@]}"; do
+		if ! is_brew_tap_installed "${tap}"; then
+			missing_taps+=("${tap}")
+		fi
+	done
 
-    if [[ ${#missing_taps[@]} -gt 0 ]]; then
-        for tap in "${missing_taps[@]}"; do
-            brew tap "${tap}"
-        done
-    fi
+	if [[ ${#missing_taps[@]} -gt 0 ]]; then
+		for tap in "${missing_taps[@]}"; do
+			brew tap "${tap}"
+		done
+	fi
 }
 
 #
 # @description Install every missing package from `BREW_PACKAGES`.
 #
 function install_brew_packages() {
-    local missing_packages=()
+	local missing_packages=()
 
-    for package in "${BREW_PACKAGES[@]}"; do
-        if ! is_brew_package_installed "${package}"; then
-            missing_packages+=("${package}")
-        fi
-    done
+	for package in "${BREW_PACKAGES[@]}"; do
+		if ! is_brew_package_installed "${package}"; then
+			missing_packages+=("${package}")
+		fi
+	done
 
-    if [[ ${#missing_packages[@]} -gt 0 ]]; then
-        if "${CI:-false}"; then
-            brew info "${missing_packages[@]}"
-        else
-            brew install --force "${missing_packages[@]}"
-        fi
-    fi
+	if [[ ${#missing_packages[@]} -gt 0 ]]; then
+		if "${CI:-false}"; then
+			brew info "${missing_packages[@]}"
+		else
+			brew install --force "${missing_packages[@]}"
+		fi
+	fi
 }
 
 #
 # @description Install every missing cask from `CASK_PACKAGES`.
 #
 function install_brew_cask_packages() {
-    local missing_packages=()
+	local missing_packages=()
 
-    for package in "${CASK_PACKAGES[@]}"; do
-        if ! is_brew_package_installed "${package}"; then
-            missing_packages+=("${package}")
-        fi
-    done
+	for package in "${CASK_PACKAGES[@]}"; do
+		if ! is_brew_package_installed "${package}"; then
+			missing_packages+=("${package}")
+		fi
+	done
 
-    if [[ ${#missing_packages[@]} -gt 0 ]]; then
-        if "${CI:-false}"; then
-            brew info --cask "${missing_packages[@]}"
-        else
-            brew install --cask --force "${missing_packages[@]}"
-        fi
-    fi
+	if [[ ${#missing_packages[@]} -gt 0 ]]; then
+		if "${CI:-false}"; then
+			brew info --cask "${missing_packages[@]}"
+		else
+			brew install --cask --force "${missing_packages[@]}"
+		fi
+	fi
 }
 
 #
 # @description Install the configured optional macOS packages and casks.
 #
 function main() {
-    # install_brew_taps
-    install_brew_packages
-    install_brew_cask_packages
+	# install_brew_taps
+	install_brew_packages
+	install_brew_cask_packages
 }
 
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
-    main
+	main
 fi
